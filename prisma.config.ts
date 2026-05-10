8eae22ceadded692b57c20d3be84f71dcda11682
aditya-raulji <hemangraulji51@gmail.com>
2026-05-10 15:37:25 +0530

    chore: fix next-auth types and prisma config for database push

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from '@prisma/config'
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
-    url: process.env.DATABASE_URL
+    url: process.env.DIRECT_URL
   }
 })
